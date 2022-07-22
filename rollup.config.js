2d2242ac4b59de5e672bf985b6aab4e75ad9a230
tamanyan <tamanyan@users.noreply.github.com>
2022-07-23 00:35:06 +0900

    update rollup.config.js

--- rollup.config.js
+++ rollup.config.js
@@ -3,7 +3,7 @@ import commonjs from '@rollup/plugin-commonjs';
 import peerDepsExternal from 'rollup-plugin-peer-deps-external';
 import typescript from 'rollup-plugin-typescript2';
 
-import pkg from '../../package.json';
+import pkg from './package.json';
 
 export default {
   input: 'src/index.ts',
