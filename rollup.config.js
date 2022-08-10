5d98ce2623f39a553da9e6bfa76d8d48dee8ed7d
Yuto Moriyasu <y.moriyasu@icloud.com>
2022-08-10 17:20:53 +0900

    change es to cjs

--- rollup.config.js
+++ rollup.config.js
@@ -10,7 +10,7 @@ export default {
   output: [
     {
       file: './dist/index.js',
-      format: 'es',
+      format: 'cjs',
     },
   ],
   external: [...Object.keys(pkg.peerDependencies || {})],
