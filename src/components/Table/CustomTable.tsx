3d40074fd215503a8abd9e30e1b2f4101b36d77f
aito <BoxPistols@users.noreply.github.com>
2022-08-30 05:27:26 +0900

    table component base

---
+++ src/components/Table/CustomTable.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export const CustomTable = () => {
+  return (
+    <>
+      <section>
+        <h2>CustomTable</h2>
+        <p>paragraph</p>
+      </section>
+    </>
+  );
+};
