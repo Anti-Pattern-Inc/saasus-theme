e8e7bbe30e98e556064a9fbeaed1b614f1f2abd4
aito <BoxPistols@users.noreply.github.com>
2022-09-14 07:07:35 +0900

    Add Component TableHeightSwitch

---
+++ src/components/Table/TableHeightSwitch.tsx
@@ -0,0 +1,26 @@
+import { ChangeEvent } from 'react'
+import { FormControlLabel, Switch } from '@mui/material'
+
+type Props = {
+  checked: boolean
+  onChange: (_event: ChangeEvent<HTMLInputElement>, checked: boolean) => void
+}
+
+export const TableHeightSwitch = ({ checked, onChange }: Props) => {
+  return (
+    <>
+      <FormControlLabel
+        control={<Switch checked={checked} onChange={onChange} />}
+        label="Full Height"
+        labelPlacement="end"
+        sx={{
+          display: 'flex',
+          flexDirection: 'row-reverse',
+          justifyContent: 'end',
+          mb: 0.25,
+          mr: 0,
+        }}
+      />
+    </>
+  )
+}
