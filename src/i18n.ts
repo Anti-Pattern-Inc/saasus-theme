fb692d095f35d7a910960c06475ccb551bb52136
ait <ait@ai.in>
2022-11-16 18:58:27 +0900

    remove 駆動テストコメント

--- src/i18n.ts
+++ src/i18n.ts
@@ -1,8 +1,6 @@
 import i18n from 'i18next'
 import { initReactI18next } from 'react-i18next'
 
-//  テストコメント2
-
 // the translations
 // (tip move them in a JSON file and import them,
 // or even better, manage them separated from your code: https://react.i18next.com/guides/multiple-translation-files)
