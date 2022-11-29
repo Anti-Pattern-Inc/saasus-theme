285d0050efcf50ca52bad536eacd6cb3fd01ae07
Yuto Moriyasu <y.moriyasu@icloud.com>
2022-11-29 14:12:37 +0900

    import components with relative path

--- src/components/Table/CustomTableFilterResult.tsx
+++ src/components/Table/CustomTableFilterResult.tsx
@@ -1,5 +1,6 @@
 import { Box, TableBody } from '@mui/material'
-import { CustomTableCell, CustomTableRow } from '@anti-pattern-inc/saasus-theme'
+import { CustomTableCell } from './CustomTableCell'
+import { CustomTableRow } from './CustomTableRow'
 
 type Props = {
   text?: string
@@ -9,7 +10,7 @@ export const CustomTableFilterResult = ({ text }: Props) => {
   return (
     <TableBody>
       <CustomTableRow>
-        {/* または {mockDatas.length} などでセルの結合。現状は多めに結合しおく */}
+        {/* または {mockDatas.length} などでセルの結合。現状は多めに結合しておく */}
         <CustomTableCell colSpan={100}>
           <Box
             sx={{
