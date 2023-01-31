d30aa58e72e2ab1952f9335e671e53014c3c515f
aito <BoxPistols@users.noreply.github.com>
2023-01-31 17:51:38 +0900

    dist

--- dist/components/SaaSusLogo/index.d.ts
+++
@@ -1,7 +0,0 @@
-declare type LogoProps = {
-    width: number;
-    alt: string;
-};
-declare function SaaSusLogo({ width, alt }: LogoProps): JSX.Element;
-export default SaaSusLogo;
-//# sourceMappingURL=index.d.ts.map
